b6ea1b65eeede5b7973907a8019a5d6368c98abd
Arthur_Silva <arthursilva.rs1@gmail.com>
2025-03-26 10:50:38 -0300

    :sparkles: feat: simple test for payment/pendent route

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,19 +1,40 @@
 import { edenTreaty } from "@elysiajs/eden";
 import { describe, expect, it } from "bun:test";
 import { app } from "../src";
+import { client } from "../src/client/types";
 
 const BASE_URL = 'http://localhost:3000'
 const api = edenTreaty<app>(BASE_URL)
 
-describe('DinamicaXP', async () => {
-    it('should return 200', async () => {
-        const {data, status} = await api.client.list.get()
-
-        if (status == 200) {
-            expect(typeof data?.clients)
-        }
+describe('DinamicaXP Pendent', async () => {
+    it('should return the user total pendent payments', async () => {
+        const { data, status } = await api.payment.pendent[1].get()
 
         expect(status).toBe(200)
+        expect(typeof data?.total).toBe("number")
+        it('the total should be equal or greater than 0', () => {
+            if (data) {
+                expect(data.total).toBeGreaterThanOrEqual(0)
+            }
+        })
     })
+    it('should return 0 if user doesnt exist', async () => {
+        const { data, status } = await api.payment.pendent[72].get()
 
+        expect(status).toBe(200)
+        expect(typeof data?.total).toBe("number")
+        if (data) {
+            expect(data.total).toBe(0)
+        }
+    })
+    it('should return error if id is not valid', async () => {
+        const list = ["dwa", -1]
+        for (const i of list) {
+            const { error } = await api.payment.pendent[i].get()
+            
+            if (error) {
+                expect(error.status).toBe(422)
+            }
+        }
+    })
 })

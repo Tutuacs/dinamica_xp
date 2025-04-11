5a514afbb9a495501179b43ba83c6b1df00149e5
Arthur_Silva <arthursilva.rs1@gmail.com>
2025-04-11 11:39:17 -0300

    :sparkles: add: ClientDb

--- src/client/repository/dbRepository.ts
+++ src/client/repository/dbRepository.ts
@@ -1,18 +1,58 @@
 import { PrismaClient } from "@prisma/client";
 import { Client } from "../types";
 
+
 export class ClientDb {
     private db: PrismaClient;
+
 
     constructor() {
         this.db = new PrismaClient();
     }
 
-    public getClient(id: number, includePayments?: boolean): Promise<Client | null> {
-        return
+
+    private toClient(client: any): Client {
+        return {
+            id: client.id,
+            cpf: client.cpf ?? undefined,
+            nascimento: client.nascimento?.toISOString().split("T")[0],
+            telefone: client.telefone ?? undefined,
+            nome: client.nome ?? "Cliente Sauna ?",
+            pendent: client.pendent ?? 0,
+            payd: client.payd ?? 0,
+            payments: client.Payment?.map((p: any) => ({
+                clientId: p.clientId,
+                paymentDate: p.paymentDate?.toISOString(),
+                unknow: p.unknow ?? undefined,
+                value: p.value,
+                payd: p.payd
+            }))
+        };
     }
 
-    public getClients(includePayments?: boolean): Promise<Client[]> {
-        return
+
+    public async getClient(id: number, includePayments?: boolean): Promise<Client | null> {
+        const client = await this.db.client.findUnique({
+            where: { id },
+            include: {
+                Payment: includePayments ?? false
+            }
+        });
+
+
+        return client ? this.toClient(client) : null;
+    }
+
+
+    public async getClients(includePayments?: boolean): Promise<Client[]> {
+        const clients = await this.db.client.findMany({
+            include: {
+                Payment: includePayments ?? false
+            }
+        });
+
+
+        return clients.map(this.toClient);
     }
 }
+
